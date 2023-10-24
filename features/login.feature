f557f6b4322814c8c88f69eb37bb7f79e7c1ddf1
MGMA1 <mayorca_3308@hotmail.com>
2023-10-23 20:05:57 -0500

    test

--- features/login.feature
+++ features/login.feature
@@ -4,7 +4,6 @@ Feature: The Internet Guinea Pig Website
 
     Given I am on the login page
     When I login with <username> and <password>
-    Then I should see a flash message saying <message>
 
     Examples:
       | username | password             | message                        |
--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -9,15 +9,11 @@ const pages = {
 }
 
 Given(/^I am on the (\w+) page$/, async (page) => {
-    await pages[page].open()
+    await LoginPage.open()
 });
 
 When(/^I login with (\w+) and (.+)$/, async (username, password) => {
     await LoginPage.login(username, password)
 });
 
-Then(/^I should see a flash message saying (.*)$/, async (message) => {
-    await expect(SecurePage.flashAlert).toBeExisting();
-    await expect(SecurePage.flashAlert).toHaveTextContaining(message);
-});
 
